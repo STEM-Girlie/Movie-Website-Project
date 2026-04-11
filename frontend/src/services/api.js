8ebfc176d6e194ddcdd607619fe0563d55a23852
STEM-girlie <nasreenbaker.creative@gmail.com>
2026-04-11 21:29:47 +0100

    fixing infinite scroll

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ const API_KEY = "2d8c891d5b6a4aa2d50bf810b57948de";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 // ⭐ Fetch popular movies with pagination
-export const getPopularMovies = async (page = 1) => {
+/*export const getPopularMovies = async (page = 1) => {
   const response = await fetch(
     `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}`,
   );
@@ -16,6 +16,23 @@ export const searchMovies = async (query, page = 1) => {
     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
       query,
     )}&page=${page}`,
+  );
+  const data = await response.json();
+  return data.results; 
+};*/
+
+// ⭐ Fetch movie details
+export const getPopularMovies = async (page = 1) => {
+  const response = await fetch(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}`,
+  );
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query, page = 1) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`,
   );
   const data = await response.json();
   return data.results;
